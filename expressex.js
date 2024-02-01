66ce0750ce2a2eb544ad795b07dc05f68713566f
tlrpv12 <park010804@gmail.com>
2024-02-02 00:49:16 +0900

    Feat: '상세보기 페이지 구현'

--- expressex.js
+++ expressex.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
 var express=require('express')
 var app=express()
@@ -12,6 +14,30 @@ app.get('/', function(request,response){
       `<a href="/create">create</a>`
     );
     response.send(html);
+  });
+});
+
+app.get('/page/:pageId',function(request,response){
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags:['h1']
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+          <a href="/update?id=${sanitizedTitle}">update</a>
+          <form action="delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    });
   });
 });
 
@@ -33,17 +59,7 @@ var app = http.createServer(function(request,response){
     var pathname= urlObj.pathname;
     if(pathname === '/'){
       if(!urlObj.searchParams.has('id')){
-        fs.readdir('./data', function(error, filelist){
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `<h2>${title}</h2>${description}`,
-            `<a href="/create">create</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
-        });
+        
       } else {
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.get('id')).base;
