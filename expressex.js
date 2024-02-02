6a41b39d740694866eb582869fbd29d6a892f50f
tlrpv12 <park010804@gmail.com>
2024-02-02 15:44:35 +0900

    Feat: 'body-parser, compression 미들웨어'

--- expressex.js
+++ expressex.js
@@ -6,6 +6,12 @@ var template = require('./lib/template.js');
 var express=require('express');
 const { request } = require('http');
 var app=express()
+var bodyParser= require('body-parser');
+var compression=require('compression');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(compression())
+
 app.get('/', function(request,response){
   fs.readdir('./data', function(error, filelist){
     var title = 'Welcome';
@@ -63,18 +69,13 @@ app.get('/create', function(request,response){
 });
 
 app.post('/create_process', function(request,response){
-  var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var title = post.title;
-        var description = post.description;
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.redirect(`/page/${title}`);
-        })
-    });
+  var post = request.body;
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.redirect(`/page/${title}`);
+      })
+  
 });
 
 app.get('/update/:pageId', function(request,response){
@@ -104,36 +105,28 @@ app.get('/update/:pageId', function(request,response){
 });
 
 app.post('/update_process',function(request,response){
-  var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.redirect(`/page/${title}`);
-            })
-          });
-      });
+  
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/page/${title}`);
+    })
+  });
+
 });
 
 app.post('/delete_process',function(request,response){
-  var body = '';
-      request.on('data', function(data){
-          body = body + data;
-      });
-      request.on('end', function(){
-          var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.redirect('/');
-          })
-      });
+  
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect('/');
+  })
+     
 });
 app.listen(3000, ()=>console.log('Example app listening on port 3000!'))
 
